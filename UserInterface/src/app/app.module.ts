393d474d7e89bc277aba27e7a41eef0693983bd5
Suzanne Gerace <SEG1129@gmail.com>
2018-12-07 16:08:07 -0500

    added ability to filter on names of recipes

--- UserInterface/src/app/app.module.ts
+++ UserInterface/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +10,13 @@ import { RecipeListComponent} from './recipeList/recipe-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UserInterface/src/app/recipeList/recipe-list.component.ts
+++ UserInterface/src/app/recipeList/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IRecipe } from './recipe';
 import { RecipeService } from './recipe.service';
 
@@ -6,27 +6,39 @@ import { RecipeService } from './recipe.service';
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   errorMessage = '';
-
   pageTitle: string = 'Recipe List';
   recipes: any[] = [];
-
   filteredRecipes: IRecipe[] = [];
-  // recipes: IRecipe[] = [];
 
-  constructor(private recipeService: RecipeService) {
+  _recipeFilter = ' ';
+  get recipeFilter(): string {
+    return this._recipeFilter;
+    }
 
+  set recipeFilter(value: string) {
+    this._recipeFilter = value;
+    this.filteredRecipes= this.recipeFilter ? this.filter(this.recipeFilter) : this.recipes
   }
 
+  constructor(private recipeService: RecipeService) {
+    this.filteredRecipes = this.recipes;
+    this.recipeFilter = ' ';
+  }
 
-ngOnInit(): void {
-  this.recipeService.getRecipes().subscribe(
-    recipes => {
-      this.recipes = recipes;
-      this.filteredRecipes = this.recipes;
-    },
-    error => this.errorMessage = <any>error
+  filter(filterBy: string): IRecipe[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.recipes.filter((recipe: IRecipe ) =>
+        recipe.Name.toLocaleLowerCase().indexOf(filterBy)!==-1)
+  }
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe(
+      recipes => {
+        this.recipes = recipes;
+        this.filteredRecipes = this.recipes;
+      },
+      error => this.errorMessage = <any>error
   );
 
 }
